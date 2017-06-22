509e471b0c559fe15aea05470b5ab47cc3d7675e
Danny Huang <kuanhsuh@gmail.com>
2017-06-22 09:19:57 +0800

    Basic Function

--- index.js
+++ index.js
@@ -27,6 +27,23 @@ app.get('/webhook/', function(req, res) {
   res.send("Wrong token")
 })
 
+app.post('/webhook/', function(req, res) {
+  let messaging_events = req.body.entry[0].messaging
+  for (let i = 0; i < messaging_events.length; i++){
+    let event = messaging_events[i]
+    let sender = event.sender.id
+    if (event.message && event.message.text) {
+      let text = event.message.text
+      sendText(sender, "Text echo: "+ text.substring(0,100))
+    }
+  }
+  res.sendStatus(200)
+})
+
+function sendText(sender, text) {
+  let messageData = {text: text}
+  sendRequest(sender, messageData)
+}
 
 app.listen(app.get('port'), function(){
   console.log('running: port')
